a4dbc07b23891fd7c632f35fa3539783aa8e9389
ItsFreSsh <lukes.fr@seznam.cz>
2023-05-08 18:14:18 +0200

    Added eta

--- main.ts
+++ main.ts
@@ -6,5 +6,15 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v1.11.0/mod.ts";
+
+const viewPath = `${Deno.cwd()}/views/`;
+
+// Set Eta's configuration
+configure({
+  // This tells Eta to look for templates
+  // In the /views directory
+  views: viewPath,
+});
 
 await start(manifest);
